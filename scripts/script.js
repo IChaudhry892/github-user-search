2fc517a63112b9d82d6f234a95b09813cb4e2704
IChaudhry892 <ibrahim.chaudhry892@gmail.com>
2025-09-22 00:32:32 -0500

    Update profile container elements with search user's data

--- scripts/script.js
+++ scripts/script.js
@@ -3,32 +3,94 @@ const usernameInput = document.getElementById("usernameInput");
 const profileContainer = document.querySelector(".profile-container");
 
 // Event Listeners
-searchButton.addEventListener("click", fetchData);
+searchButton.addEventListener("click", loadUserProfile);
 usernameInput.addEventListener("keypress", (e) => {
     if (e.key == "Enter")
-        fetchData();
+        loadUserProfile();
 })
 
-// GitHub API fetch function
-async function fetchData() {
-    try {
-        const username = usernameInput.value.toLowerCase();
+// Profile Handler Function
+async function loadUserProfile() {
+    const username = usernameInput.value.toLowerCase();
 
+    try {
+        const data = await fetchData(username);
+        updateProfileContainer(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// GitHub API Fetch Function
+async function fetchData(username) {
+    try {
         const response = await fetch(`https://api.github.com/users/${username}`);
 
         if (!response.ok) {
             throw new Error("Could not fetch resource");
         }
-
-        const data = await response.json();
-        console.log(data);
-        const userAvatar = data.avatar_url;
-        const imgElement = document.getElementById("userAvatar");
-
-        imgElement.src = userAvatar;
-        profileContainer.style.display = "flex";
-    }
-    catch(error) {
+        return await response.json();
+    } catch (error) {
         console.error(error);
     }
+}
+
+// Update Profile Container Function
+function updateProfileContainer(data) {
+    const userAvatar = document.getElementById("userAvatar");
+    userAvatar.src = data.avatar_url;
+
+    const profileUsername = document.getElementById("profileUsername");
+    profileUsername.textContent = `@${data.login}`;
+
+    const profileCreatedDate = document.getElementById("profileCreatedDate");
+    const createdDate = new Date(data.created_at);
+    profileCreatedDate.textContent = `Member Since ${createdDate.toDateString()}`;
+
+    const publicReposCount = document.getElementById("publicReposCount");
+    publicReposCount.textContent = data.public_repos;
+
+    const followersCount = document.getElementById("followersCount");
+    followersCount.textContent = data.followers;
+
+    const followingCount = document.getElementById("followingCount");
+    followingCount.textContent = data.following;
+
+    const locationValue = document.getElementById("locationValue");
+    if (data.location) {
+        document.getElementById("locationLabel").style.color = "var(--color-text)";
+        locationValue.style.color = "var(--color-text)";
+        locationValue.textContent = data.location;
+    } else {
+        locationValue.textContent = "Not Available";
+    }
+
+    const twitterHandleValue = document.getElementById("twitterHandleValue");
+    if (data.twitter_username) {
+        document.getElementById("twitterHandleLabel").style.color = "var(--color-text)";
+        twitterHandleValue.style.color = "var(--color-text)";
+        twitterHandleValue.textContent = `@${data.twitter_username}`;
+    } else {
+        twitterHandleValue.textContent = "Not Available";
+    }
+
+    const blogValue = document.getElementById("blogValue");
+    if (data.blog) {
+        document.getElementById("blogLabel").style.color = "var(--color-text)";
+        blogValue.style.color = "var(--color-text)";
+        blogValue.textContent = data.blog;
+    } else {
+        blogValue.textContent = "Not Available";
+    }
+
+    const companyValue = document.getElementById("companyValue");
+    if (data.company) {
+        document.getElementById("companyLabel").style.color = "var(--color-text)";
+        companyValue.style.color = "var(--color-text)";
+        companyValue.textContent = data.company;
+    } else {
+        companyValue.textContent = "Not Available";
+    }
+
+    profileContainer.style.display = "flex";
 }
